cd1e38e9bd0e93cc826ee9ffb3ebe0b0597e484b
ddyfedd <dzsamboki.techwriter@gmail.com>
2021-03-28 17:51:10 +0200

    #3 func test lesson

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -35,10 +35,14 @@ suite("Functional Tests", function () {
       chai
         .request(server)
         .put("/travellers")
-
+        .send({ surname: 'Colombo' })
         .end(function (err, res) {
-          assert.fail();
+          assert.equal(res.status, 200, 'response should be 200');
+          assert.equal(res.type, 'application/json', 'response type should be json');
+          assert.equal(res.body.name, 'Cristoforo', 'res.body.name should be "Cristoforo"');
+          assert.equal(res.body.surname, 'Colombo', 'res.body.surname should be "Colombo"');
 
+          
           done();
         });
     });
