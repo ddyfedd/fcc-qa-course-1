ffda61d395d9abe12b4388ed2e63dd2e2e7c26e6
ddyfedd <dzsamboki.techwriter@gmail.com>
2021-03-28 18:11:55 +0200

    #4 correction 2

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -50,13 +50,13 @@ suite("Functional Tests", function () {
     test('send {surname: "da Verrazzano"}', function (done) {
       chai
         .request(server)
-        .put("/travelers")
+        .put('/travelers')
         .send({ surname: 'da Verrazzano' })
         .end(function (err, res) {
           assert.equal(res.status, 200, 'response status should be 200');
           assert.equal(res.type, 'application/json', 'response type should be json');
-          assert.equal(res.body.name, 'Giovanni', 'res.body.name should be "Giovanni"');
-          assert.equal(res.body.surname, 'da Verrazzano', 'res.body.surname should be "da Verrazzano"');
+          assert.equal(res.body.name, 'Giovanni');
+          assert.equal(res.body.surname, 'da Verrazzano');
       
           done();
         });
